389c679e11df236915933d935e60c5f729a314cf
Xavier ROGNON <xavier.rognon@epitech.eu>
2024-05-30 19:48:05 +0200

    [+][Refactor] GuiSocket is now a static class

--- gui/src/Utils/GuiSocket.cpp
+++ gui/src/Utils/GuiSocket.cpp
@@ -11,98 +11,67 @@
 #include <exception>
 #include <iostream>
 
-Zappy::GuiSocket::GuiSocket()
-{
-    createSocket();
-}
-
-Zappy::GuiSocket::~GuiSocket()
-{
-    closeSocket();
-}
-
-int Zappy::GuiSocket::getSocket() const
-{
-    return _socket;
-}
-
-int &Zappy::GuiSocket::getSocket()
-{
-    return _socket;
-}
-
-void Zappy::GuiSocket::run(std::string port, std::string addr)
-{
-    std::string response;
-    std::string team = "GRAPHICS";
-    int fd = 0;
-
-    connectSocket(port, addr);
-    while (true) {
-        selectSocket();
-        fd = getSocket();
-        fd >> response;
-        if (response == "WELCOME\n")
-            fd << team;
-        std::cout << "response -> " << response << std::endl;
-    }
-}
-
 extern "C" {
-
-    void Zappy::GuiSocket::createSocket(void)
-    {
-        _socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (_socket == -1)
-            Zappy::ErrorGUI(SOCKET_ERROR, "socket: " + std::string(strerror(errno)));
-    }
-
-    int Zappy::GuiSocket::selectSocket(void)
-    {
-        int ret = 0;
-
-        FD_ZERO(&_rfds);
-        FD_ZERO(&_wfds);
-        FD_SET(_socket, &_rfds);
-        ret = select(_socket + 1, &_rfds, &_wfds, NULL, NULL);
-        if (ret == -1)
-            throw Zappy::ErrorGUI(SOCKET_ERROR, "select: " + std::string(strerror(errno)));
-        return ret;
-    }
-
-    void Zappy::GuiSocket::closeSocket(void)
-    {
-        close(_socket);
-    }
-
-    void Zappy::GuiSocket::connectSocket(const std::string port, const std::string serverAdress)
+    std::size_t Zappy::GuiSocket::createSocket(const std::size_t &port, const std::string &ip)
     {
         struct sockaddr_in sock_serv;
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-        std::cout << port << std::endl;
-        std::cout << serverAdress << std::endl;
-
+        if (sock > 65535 || sock < 0)
+            Zappy::ErrorGUI(SOCKET_ERROR, "socket: " + std::string(strerror(errno)));
         sock_serv.sin_family = AF_INET;
-        sock_serv.sin_port = htons(atoi(port.c_str()));
-        sock_serv.sin_addr.s_addr = inet_addr(serverAdress.c_str());
-        if (connect(_socket, (struct sockaddr *) &sock_serv, sizeof(sock_serv)) == -1)
+        sock_serv.sin_port = htons(port);
+        sock_serv.sin_addr.s_addr = inet_addr(ip.c_str());
+        if (connect(sock, (struct sockaddr *) &sock_serv, sizeof(sock_serv)) == -1)
             throw ErrorGUI(SOCKET_ERROR, "connect: " + std::string(strerror(errno)));
+        return sock;
+    }
+
+
+    void Zappy::GuiSocket::closeSocket(const std::size_t &socket)
+    {
+        close(socket);
+    }
+
+    void Zappy::GuiSocket::sendToServer(const std::size_t &socket, const std::string &msg)
+    {
+        if (send(socket, msg.c_str(), sizeof(char) * (msg.length() + 1), 0) == -1)
+            throw Zappy::ErrorGUI(SOCKET_ERROR, "send: " + std::string(strerror(errno)));
+        std::cout << "sent: " << msg << std::endl;
+    }
+
+    std::string Zappy::GuiSocket::receiveFromServer(const std::size_t &socket)
+    {
+        char buffer;
+        std::string newBuff;
+
+        if (selectRead(socket) == -1)
+            return "";
+        while (read(socket, &buffer, sizeof(buffer))) {
+            if (buffer == '\n')
+                break;
+            newBuff += buffer;
+        }
+        std::cout << "received: " << newBuff << std::endl;
+        return newBuff;
+    }
+
+    int Zappy::GuiSocket::selectRead(const std::size_t &sock)
+    {
+        int ret = 0;
+        struct timeval timeoutStruct;
+        fd_set _rfds;
+
+        FD_SET(sock, &_rfds);
+        timeoutStruct.tv_sec = 0;
+        timeoutStruct.tv_usec = 100;
+        ret = select(sock + 1, &_rfds, NULL, NULL, &timeoutStruct);
+        if (ret == -1)
+            throw Zappy::ErrorGUI(SOCKET_ERROR, "select: " + std::string(strerror(errno)));
+        // if (FD_ISSET(sock, &_rfds)) {
+        //     return -1;
+        // }
+        FD_ZERO(&_rfds);
+        return ret;
     }
 };
-
-int &Zappy::operator<<(int &sock, const std::string &val)
-{
-    if (write(sock, val.c_str(), sizeof(char) * (val.length() + 1)) == -1)
-        throw Zappy::ErrorGUI(SOCKET_ERROR, "write: " + std::string(strerror(errno)));
-    return sock;
-}
-
-int &Zappy::operator>>(int &sock, std::string &val)
-{
-    char buffer[40000];
-
-    if (read(sock, buffer, sizeof(char) * 40000) == -1)
-        throw Zappy::ErrorGUI(SOCKET_ERROR, "read: " + std::string(strerror(errno)));
-    val = std::string(buffer);
-    return sock;
-}
