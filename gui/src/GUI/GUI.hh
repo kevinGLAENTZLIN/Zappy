f70ac9017ca82eb302604e9a5338d235c0a8e0ca
Xavier ROGNON <xavier.rognon@epitech.eu>
2024-05-30 20:22:08 +0200

    [+][Feature] adding the GUI class with connection to the server, argument handling and gameloop

--- gui/src/GUI/GUI.hh
+++ gui/src/GUI/GUI.hh
@@ -8,11 +8,29 @@
 
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include "CommonElements/CommonElements.hh"
+#include "../Raylib/Window/Window.hh"
+#include "Scene/IScene.hh"
+#include "Scene/SceneFactory.hh"
+
 namespace Zappy {
     class GUI {
     public:
-
+        GUI(const std::vector<std::string> &args);
+        ~GUI();
     private:
+        void handleArguments(const std::vector<std::string> &args);
+        void runGUI();
+        void initScenes();
 
+        std::shared_ptr<CommonElements> _commonElements;
+        std::unique_ptr<Raylib::Window> _window;
+        std::vector<std::unique_ptr<IScene>> _scenes;
+        std::size_t _port;
+        std::string _ip;
     };
 }
