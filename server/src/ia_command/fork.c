34315056abdf636c82dda4ea145652c7cb8b18fa
Gabin <gabin.pinos@epitech.eu>
2024-05-31 21:30:34 +0200

    [+][Feat] set command Pfk Pdr & Pgt

--- server/src/ia_command/fork.c
+++ server/src/ia_command/fork.c
@@ -16,6 +16,7 @@ void my_fork(server_t *server, int i, char *input)
         team = get_team_by_name(server, PLAYER->team_name);
         push_back_egg(ZAPPY, PLAYER->x, PLAYER->y, team);
         team->nb_max_player += 1;
+        send_to_all_gui(server, "pfk #%d\n", PLAYER->id);
         dprintf(FD_CLIENT, "ok\n");
     }
 }
--- server/src/ia_command/set.c
+++ server/src/ia_command/set.c
@@ -7,28 +7,52 @@
 
 #include "../../include/server.h"
 
+static void handle_set_response(server_t *server, int i, char *object)
+{
+    if (strcmp(object, "food") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 0);
+    if (strcmp(object, "linemate") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 1);
+    if (strcmp(object, "deraumere") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 2);
+    if (strcmp(object, "sibur") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 3);
+    if (strcmp(object, "mendiane") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 4);
+    if (strcmp(object, "phiras") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 5);
+    if (strcmp(object, "thystame") == 0)
+        send_to_all_gui(server, "pdr #%d %d", PLAYER->id, 6);
+    dprintf(FD_CLIENT, "ok\n");
+}
+
+static void set_object3(server_t *server, int i, char *object, tile_t *tile)
+{
+    if (strcmp(object, "thystame") == 0 && PLAYER->thystame > 0) {
+        PLAYER->thystame -= 1;
+        tile->thystame += 1;
+        return handle_set_response(server, i, object);
+    }
+    return (void)dprintf(FD_CLIENT, "ko\n");
+}
+
 static void set_object2(server_t *server, int i, char *object, tile_t *tile)
 {
-    bool tmp = (strcmp(object, "thystame") == 0 && PLAYER->thystame > 0);
-
     if (strcmp(object, "sibur") == 0 && PLAYER->sibur > 0) {
         PLAYER->sibur -= 1;
         tile->sibur += 1;
-        return (void)dprintf(FD_CLIENT, "ok\n");
+        return handle_set_response(server, i, object);
     }
     if (strcmp(object, "mendiane") == 0 && PLAYER->mendiane > 0) {
         PLAYER->mendiane -= 1;
         tile->mendiane += 1;
-        return (void)dprintf(FD_CLIENT, "ok\n");
+        return handle_set_response(server, i, object);
     }
     if (strcmp(object, "phiras") == 0 && PLAYER->phiras > 0) {
         PLAYER->phiras -= 1;
         tile->phiras += 1;
-        return (void)dprintf(FD_CLIENT, "ok\n");
+        return handle_set_response(server, i, object);
     }
-    PLAYER->thystame -= tmp ? 1 : 0;
-    tile->thystame += tmp ? 1 : 0;
-    return (void)dprintf(FD_CLIENT, tmp ? "ok\n" : "ko\n");
 }
 
 static void set_object(server_t *server, int i, char *object)
@@ -40,17 +64,17 @@ static void set_object(server_t *server, int i, char *object)
     if (strcmp(object, "food") == 0 && PLAYER->food > 0) {
         PLAYER->food -= 1;
         tile->food += 1;
-        return (void)dprintf(FD_CLIENT, "ok\n");
+        return handle_set_response(server, i, object);
     }
     if (strcmp(object, "linemate") == 0 && PLAYER->linemate > 0) {
         PLAYER->linemate -= 1;
         tile->linemate += 1;
-        return (void)dprintf(FD_CLIENT, "ok\n");
+        return handle_set_response(server, i, object);
     }
     if (strcmp(object, "deraumere") == 0 && PLAYER->deraumere > 0) {
         PLAYER->deraumere -= 1;
         tile->deraumere += 1;
-        return (void)dprintf(FD_CLIENT, "ok\n");
+        return handle_set_response(server, i, object);
     }
     set_object2(server, i, object, tile);
 }
