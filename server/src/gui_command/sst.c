37d4590c53b914a2ff881e83e32f5d4947653f7a
Gabin <gabin.pinos@epitech.eu>
2024-05-31 21:58:43 +0200

    [+][Feat] Set commands Sgt & Sst

--- server/src/gui_command/sst.c
+++ server/src/gui_command/sst.c
@@ -11,7 +11,11 @@ void sst(server_t *server, int i, char *input)
 {
     char **tab = get_parameters(input);
 
-    (void) server;
-    (void) i;
+    if (tab == NULL || is_number(tab[0])) {
+        free_tab(tab);
+        return (void)dprintf(FD_CLIENT, "sbp\n");
+    }
+    ZAPPY->frequence = atoi(tab[0]);
+    dprintf(FD_CLIENT, "sst %d\n", ZAPPY->frequence);
     free_tab(tab);
 }
