71cb8cb7726dfd18919b474333aaa318de5e28e4
Gabin <gabin.pinos@epitech.eu>
2024-06-04 17:43:59 +0200

    [+][Feat] Error handling on the Sst command

--- server/src/gui_command/sst.c
+++ server/src/gui_command/sst.c
@@ -10,12 +10,15 @@
 void sst(server_t *server, int i, char *input)
 {
     char **tab = get_parameters(input);
+    int tmp = ZAPPY->frequence;
 
     if (tab == NULL || is_number(tab[0])) {
         free_tab(tab);
         return (void)send_client(FD_CLIENT, "sbp\n");
     }
     ZAPPY->frequence = atoi(tab[0]);
+    if (ZAPPY->frequence < 0 || ZAPPY->frequence > 2000)
+        ZAPPY->frequence = tmp;
     send_to_all_gui(server, "sst %d\n", ZAPPY->frequence);
     free_tab(tab);
 }
