52b541ed588d8239bc1214d103b3fce3ee044918
Xavier ROGNON <xavier.rognon@epitech.eu>
2024-06-10 15:56:35 +0200

    [+][Feature] adding a way to detext if the tile is being clicked

--- gui/src/GUI/Scene/Game/Map/Tiles.hh
+++ gui/src/GUI/Scene/Game/Map/Tiles.hh
@@ -20,11 +20,18 @@ namespace Zappy {
         Tiles(int x, int y, Vector2 mapSize,
               std::vector<std::shared_ptr<Raylib::Model3D>> models);
         ~Tiles();
+
         void setResources(std::vector<std::size_t> resourcesQuantity);
+
+        Vector2 getIndex() const;
+        std::vector<std::size_t> getResources() const;
+
+        bool Hits(Ray mouseRay);
         void Draw();
     private:
         Vector2 _index;
         Vector2 _mapSize;
+        BoundingBox _bounds;
         std::vector<std::shared_ptr<Raylib::Model3D>> _models;
         std::vector<std::size_t> _resourcesQuantity;
     };
