8521f382fecf05b2a1a790e2b1baa3cdc0bf9a8c
Gabin <gabin.pinos@epitech.eu>
2024-06-14 12:23:41 +0200

    [+][Feat] Send some server info

--- server/src/signal.c
+++ server/src/signal.c
@@ -14,6 +14,7 @@ void teams_sigint(int signum)
 {
     (void) signum;
     free_server(server);
+    printf("\033[1;31m[INFO]\033[0m: Shutting down the server\n");
     server = NULL;
     exit(0);
 }
